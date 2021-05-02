808215d42c86daeccd7289dcc4879b57af2efc1a
Aescosaurus <michaelbuslik@hotmail.com>
2021-05-02 10:11:38 -0600

    Finish up last level and use it for both tools.

---
+++ Assets/Code/TextSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSetter
+    :
+    MonoBehaviour
+{
+    void Start()
+	{
+        if( PlayerPrefs.GetInt( "has_jetpack",0 ) > 0 )
+		{
+			GetComponent<TextMesh>().text = "Hold space to use jetpack";
+		}
+	}
+}
